03e4617eaab9a1bfc4cc0649f46ae4f59810e136
FastBery <Falls.ignatev@mail.ru>
2023-10-24 13:03:27 +0300

    fixed string_to_vector and added some stuff

--- Infinite_number/my_infinite_int_number.h
+++ Infinite_number/my_infinite_int_number.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using u = int;
 
@@ -10,46 +11,6 @@ class inf
 {
     std::vector<u> vec;
     /* if True number < 0; if False number >= 0*/
-public:
-    ~inf()
-    {
-        vec.clear();
-    }
-
-    inf()
-        : vec()
-    {}
-
-    inf(std::string str)
-        : vec()
-    {
-        vec = string_to_vector(str);
-    }
-
-    std::vector<u> string_to_vector(std::string str){
-        std::size_t size = str.size();
-        std::size_t n = size/4;
-        std::vector<u> answer;
-        for(std::size_t i = 0; i < n; i++){
-            std::vector<u> num;
-            for(std::size_t j = 0; j < 4; j++){
-                num.insert(num.begin(), str[size - i*4 - j - 1] - 48);
-            }
-            answer.insert(answer.begin(), num.begin(), num.end());
-        }
-        std::vector<u> num;
-        for(std::size_t i = 0; i < size - 4*n; i++){
-            num.insert(num.begin(), str[size - 4*n - i - 1] - 48);
-        }
-        answer.insert(answer.begin(), num.begin(), num.end());
-        return answer;
-    }
-
-    inf &operator+(inf &other){
-        inf answer = sum_of_2_vectors(this->vec, (&other)->vec);
-        return answer;
-    }
-
     std::vector<u> sum_of_2(u a, u b){
         std::vector<u> vec = {0, 0};
         if(a >= 10000 or b >= 10000){
@@ -119,11 +80,258 @@ public:
         if(x != 0){
             answer.insert(answer.begin(), x);
         }
-        inf ans;
-        ans->vec = answer;
-        return ans;
+        // inf ans;
+        // ans.vec = answer;
+        return answer;
     }
+
+    u get_sign(std::vector<u> a){return a.front() < 0 ? -1 : 1;}
+
+    std::vector<u> dif_of_2_vectors(std::vector<u> a, std::vector<u> b){
+        u sign_a = get_sign(a), sign_b = get_sign(b);
+        if(sign_a == -1 and sign_b == 1){
+            a.front() *= -1;
+            auto answer = sum_of_2_vectors(a, b);
+            answer.front() *= -1;
+            return answer;
+        }
+        if(sign_a == 1 and sign_b == -1){
+            b.front() *= -1;
+            return sum_of_2_vectors(a, b);
+        }
+        if(sign_a == -1 and sign_b == -1){
+            b.front() *= -1;
+            a.front() *= -1;
+            auto answer = dif_of_2_vectors(b, a);
+            return answer;
+        }
+        if(max_of_2(a, b) == a){
+            b.front() *= -1;
+            return sum_of_2_vectors(a, b);
+        }
+        else{
+            a.front() *= -1;
+            auto answer = sum_of_2_vectors(b, a);
+            answer.front() *= -1;
+            return answer;
+        }
+    }
+    
+    std::vector<u> max_of_2(std::vector<u> a, std::vector<u> b){
+        std::vector<u> answer;
+        if(a.size() != b.size()){
+            if(a.size() > b.size()){
+                return a;
+            }
+            else{
+                return b;
+            }
+        }
+        else{
+            for(int i = 0; i < a.size(); i++){
+                if(abs(a[i]) > abs(b[i])){
+                    return a;
+                }
+                else{
+                    return b;
+                }
+            }
+        }
+        return answer;
+    }
+
+public:
+    ~inf()
+    {
+        vec.clear();
+    }
+
+    inf()
+        : vec()
+    {}
+
+    inf(std::string str)
+        : vec()
+    {
+        vec = string_to_vector(str);
+    }
+
+    std::vector<u> string_to_vector(std::string str){
+        u sign = 1;
+        std::size_t size = str.size();
+        if(str[0] == '-'){
+            size--;
+            sign = -1;
+        }
+        std::size_t n = size/4;
+        std::vector<u> answer;
+        for(std::size_t i = 0; i < n; i++){
+            u num = 0;
+            for(std::size_t j = 0; j < 4; j++){
+                // num.insert(num.begin(), str[size - i*4 - j - 1] - 48);
+                num += pow(10, j)*(str[size - i*4 - j - 1] - 48);
+            }
+            answer.insert(answer.begin(), num);
+        }
+        u num = 0;
+        for(std::size_t i = 0; i < size - 4*n; i++){
+            num += pow(10, i)*(str[size - 4*n - i - 1] - 48);
+        }
+        answer.insert(answer.begin(), num);
+        answer.front() *= sign;
+        return answer;
+    }
+
+    inf &operator+(inf &other){
+        // inf answer;
+        this->vec = sum_of_2_vectors(this->vec, other.vec);
+        // this = *answer;
+        return *this;
+    }
+
+    inf &operator+(inf &&other){
+        this->vec = sum_of_2_vectors(this->vec, other.vec);
+        return *this;
+    }
+
+    inf &operator-(inf &other){
+        this->vec = dif_of_2_vectors(this->vec, other.vec);
+        return *this;
+    }
+
+    inf &operator-(inf &&other){
+        this->vec = dif_of_2_vectors(this->vec, other.vec);
+        return *this;
+    }
+
+    inf &operator=(inf &other){
+        this->vec = other.vec;
+        return *this;
+    }
+
+    inf &operator=(inf &&other){
+        this->vec = other.vec;
+        return *this;
+    }
+
+    inf &operator=(std::string &str){
+        this->vec = string_to_vector(str);
+        return *this;
+    }
+
+    inf &operator=(std::string &&str){
+        this->vec = string_to_vector(str);
+        return *this;
+    }
+
+    inf &operator=(std::vector<u> &a){
+        this->vec = a;
+        return *this;
+    }
+
+    inf &operator=(std::vector<u> &&a){
+        this->vec = a;
+        return *this;
+    }
+
+    u &operator[](std::size_t &n){
+        return this->vec[n];
+    }
+
+    u &operator[](std::size_t &&n){
+        return this->vec[n];
+    }
+
+    bool operator==(inf const &other) const {
+        if(this->vec.size() == other.vec.size()){
+            for(std::size_t i = 0; i < other.vec.size(); i++){
+                if(this->vec[i] != other.vec[i]){
+                    return false;
+                }
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool operator!=(inf const &other) const {
+        if(*this == other){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    bool operator>(inf const &other) const {
+        if(*this != other){
+            if(this->vec.size() > other.vec.size()){
+                return true;
+            }
+            else{
+                if(this->vec.size() < other.vec.size()){
+                    return false;
+                }
+                else{
+                    for(std::size_t i = 0; i < other.vec.size(); i++){
+                        if(this->vec[i] > other.vec[i]){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            return false;
+        }
+    }
+    bool operator<(inf const &other) const {
+        if(*this == other or *this > other){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    bool operator<=(inf const &other) const {
+        if(*this == other or *this < other){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool operator>=(inf const &other) const {
+        if(*this < other){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    inf       *begin()       {return this;}
+    inf       *  end()       {return this;}
+    inf const *begin() const {return this;}
+    inf const *  end() const {return this;}
+
+    std::size_t size(){
+        std::cout << this->vec.size() << std::endl;
+        return this->vec.size();
+    }
+
 };
+
+std::ostream &operator<<(std::ostream& os, inf const &a){
+    // std::size_t size = ;
+    // for(std::size_t i = 0; i < a.size(); i++){
+    //     os << a[i];
+    // }
+    return os;
+}
 
 // template<typename T>
 // void inf<T>::reallocate(u64 size){
---
+++ Infinite_number/temp.cpp
@@ -0,0 +1,47 @@
+#include "my_infinite_int_number.h"
+#include <iostream>
+
+using namespace std;
+
+// std::vector<u> string_to_vector(std::string str){
+//         std::size_t size = str.size();
+//         std::size_t n = size/4;
+//         std::vector<u> answer;
+//         for(std::size_t i = 0; i < n; i++){
+//             u num = 0;
+//             for(std::size_t j = 0; j < 4; j++){
+//                 // num.insert(num.begin(), str[size - i*4 - j - 1] - 48);
+//                 num += pow(10, j)*(str[size - i*4 - j - 1] - 48);
+//             }
+//             answer.insert(answer.begin(), num);
+//         }
+//         u num = 0;
+//         for(std::size_t i = 0; i < size - 4*n; i++){
+//             num += pow(10, i)*(str[size - 4*n - i - 1] - 48);
+//         }
+//         answer.insert(answer.begin(), num);
+//         return answer;
+//     }
+
+int main(){
+    inf a;
+    a = "-123455";
+    inf  b;
+    b = "543211";
+    // a = {1, 2342} + {2, 2102};
+    // for(std::size_t i = 0; i < 2; i++){
+    //     cout << a[i];
+    // }
+    cout << a.size() << endl;
+    // vector<u> vec = string_to_vector("123456");
+    // for(auto now : vec){
+    //     cout << now << " ";
+    // }
+    cout << a[0] << " " << a[1] << endl;
+    cout << b[0] << b[1] << endl;
+    inf c;
+    c = b - a;
+    cout << c[0] << c[1] << endl;
+    // inf c = a + b;
+    cout << c[0] << c[1] << endl;
+}
